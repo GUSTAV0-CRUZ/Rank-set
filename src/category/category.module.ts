aa2d3564f5da8d50a90b8306e7de82d9dd402969
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-02-28 20:56:49 -0300

    feat(category): adiciona as dependencias no module category para usar o schema e o repository

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from './schemas/category.schema';
+import { CategoryRepository } from './repository/category.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+  ],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService, CategoryRepository],
 })
 export class CategoryModule {}
