5529a74d3d3e4ff0065a26687dfc3570a20a8e12
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-02-28 13:03:32 -0300

    feat(category): define a entidade category

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,14 @@
-export class Category {}
+import { Player } from 'src/player/entities/Player.entitie';
+
+export class Category {
+  name: string;
+  descripion: string;
+  events: Event[];
+  players: Player[];
+}
+
+export interface Evento {
+  name: string;
+  operation: string;
+  value: number;
+}
