fdbf4c6a3a268272cddae992deabd5c4bc7dcb33
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-02-28 15:27:58 -0300

    feat(category): add os dtos

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,28 @@
-export class CreateCategoryDto {}
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { EventOfCategory } from '../entities/category.entity';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(150)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  descripion: string;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayMinSize(1)
+  events: EventOfCategory[];
+}
--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -1,4 +1,11 @@
+import { IsMongoId, IsOptional } from 'class-validator';
 import { CreateCategoryDto } from './create-category.dto';
 import { PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @IsOptional()
+  @IsMongoId({ each: true })
+  @Type(() => Array)
+  players: string[] | string;
+}
