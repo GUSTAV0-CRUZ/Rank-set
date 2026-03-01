faeb7e6ff214f9eb7666569eff2526d9badd6d90
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-02-28 23:30:14 -0300

    feat(category): adiciona a criacao e a busca de todas categorias

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoryRepository } from './repository/category.repository';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    try {
+      const player = await this.categoryRepository.create(createCategoryDto);
+
+      return player;
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      const erroFildRequired: string | undefined = error?.message?.split(
+        'Path',
+      )[1] as string;
+      if (erroFildRequired)
+        throw new BadRequestException(`Fild:${erroFildRequired}`);
+      if (JSON.stringify(error.keyPattern) === JSON.stringify({ name: 1 }))
+        throw new BadRequestException('Value of key name is duplicate');
+
+      throw new BadRequestException(error.message);
+    }
   }
 
   findAll() {
-    return `This action returns all category`;
+    return this.categoryRepository.findAll();
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} category`;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} ${updateCategoryDto.name} category`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} category`;
   }
 }
