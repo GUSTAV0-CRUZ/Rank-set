2f41c537391c847d2902dbd5f7bf025ce3b4cb7d
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-03 11:49:47 -0300

    feat(challenge): adiciona o repository

--- src/challenge/challenge.module.ts
+++ src/challenge/challenge.module.ts
@@ -3,12 +3,13 @@ import { ChallengeService } from './challenge.service';
 import { ChallengeController } from './challenge.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChallengeSchema } from './schema/challenge.schema';
+import { ChallengeRepository } from './repository/challenge.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Challenge', schema: ChallengeSchema }]),
   ],
   controllers: [ChallengeController],
-  providers: [ChallengeService],
+  providers: [ChallengeService, ChallengeRepository],
 })
 export class ChallengeModule {}
