dcdb0841ac33e042b3b0e9b14d8a0f024a94afc9
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-03 11:51:43 -0300

    feat(challenge): cria o schema

---
+++ src/challenge/schema/challenge.schema.ts
@@ -0,0 +1,59 @@
+import { Player } from 'src/player/entities/Player.entitie';
+import { ChallengeStatus } from '../enums/challenge-status.enum';
+import { Challenge, Match } from '../entities/challenge.entity';
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+@Schema({ timestamps: true })
+export class MatchSubSchemaDb implements Match {
+  category: string;
+  players: Player[];
+  def: Player;
+  result: Result[];
+}
+
+export const MatchSubSchema = SchemaFactory.createForClass(MatchSubSchemaDb);
+
+export type ChallengeDocument = Document & ChallengeSchemaDb;
+
+@Schema({ timestamps: true })
+export class ChallengeSchemaDb implements Challenge {
+  @Prop({ type: Date })
+  dateHoursChallenge: Date;
+
+  @Prop({ type: Date })
+  dateHoursRequest: Date;
+
+  @Prop({ type: Date })
+  dateHoursResponse: Date;
+
+  @Prop({
+    type: String,
+    enum: ChallengeStatus,
+    default: ChallengeStatus.PENDING,
+  })
+  status: ChallengeStatus;
+
+  @Prop({ type: mongoose.Schema.ObjectId, ref: 'players' })
+  applicant: Player;
+
+  @Prop({ type: String })
+  category: string;
+
+  @Prop([
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'players',
+    },
+  ])
+  players: Player[];
+
+  @Prop({ type: mongoose.Schema.ObjectId, ref: 'matchs' })
+  match: Match;
+}
+
+export class Result {
+  set: string;
+}
+
+export const ChallengeSchema = SchemaFactory.createForClass(ChallengeSchemaDb);
