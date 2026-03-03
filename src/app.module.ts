2ea77de2e3cc496cc16d25e5733c60fee51ae0f1
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-02 22:19:18 -0300

    feat(challenge): Nova entidade

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PlayerModule } from './player/player.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
+import { ChallengeModule } from './challenge/challenge.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CategoryModule } from './category/category.module';
     PlayerModule,
     MongooseModule.forRoot(String(process.env.MONGODB_CONECTION)),
     CategoryModule,
+    ChallengeModule,
   ],
   controllers: [],
   providers: [],
