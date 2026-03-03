7b6bafd0c3d693d23f347129668ceab72522a3fb
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-02 23:08:20 -0300

    feat(challenge): define a entidade Challenge

--- src/challenge/entities/challenge.entity.ts
+++ src/challenge/entities/challenge.entity.ts
@@ -1,1 +1,24 @@
-export class Challenge {}
+import { Player } from 'src/player/entities/Player.entitie';
+import { ChallengeStatus } from '../enums/challenge-status.enum';
+
+export class Challenge {
+  dateHoursChallenge: Date;
+  dateHoursRequest: Date;
+  dateHoursResponse: Date;
+  status: ChallengeStatus;
+  applicant: Player;
+  category: string;
+  players: Player[];
+  match: Match;
+}
+
+export class Match {
+  category: string;
+  players: Player[];
+  def: Player;
+  result: Result[];
+}
+
+export class Result {
+  set: string;
+}
