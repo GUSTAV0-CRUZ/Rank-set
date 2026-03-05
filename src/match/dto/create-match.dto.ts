179a030c14b465e552673fd6b5df4da8aa358e83
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-05 10:47:06 -0300

    feat(match): cria os dtos

--- src/match/dto/create-match.dto.ts
+++ src/match/dto/create-match.dto.ts
@@ -1,1 +1,21 @@
-export class CreateMatchDto {}
+import { Type } from 'class-transformer';
+import { IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Result } from '../entities/match.entity';
+
+export class CreateMatchDto {
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @IsNotEmpty()
+  @IsOptional()
+  @IsMongoId({ each: true })
+  @Type(() => Array)
+  players: string[] | string;
+
+  @IsMongoId({ each: true })
+  def: string;
+
+  @Type(() => Array<Result>)
+  result: Result[];
+}
--- src/match/dto/update-match.dto.ts
+++ src/match/dto/update-match.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMatchDto } from './create-match.dto';
+import { Type } from 'class-transformer';
+import { IsMongoId } from 'class-validator';
+import { Result } from '../entities/match.entity';
 
-export class UpdateMatchDto extends PartialType(CreateMatchDto) {}
+export class UpdateMatchDto {
+  @IsMongoId({ each: true })
+  def: string;
+
+  @Type(() => Array<Result>)
+  result: Result[];
+}
