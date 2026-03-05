7edf19ce867a8b67e3755a2403fc78c7a2033be7
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-05 12:13:17 -0300

    feat(match): adiciona a busca de todos os matchs e de apenas um por id

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,28 +1,46 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
 import { MatchRepository } from './repository/match.repository';
+import { Match } from './entities/match.entity';
 
 @Injectable()
 export class MatchService {
   constructor(private readonly matchRepository: MatchRepository) {}
 
-  async create(createMatchDto: CreateMatchDto) {
+  async create(createMatchDto: CreateMatchDto): Promise<Match> {
     try {
       const match = await this.matchRepository.create(createMatchDto);
       return match;
     } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       throw new BadRequestException(error.message);
     }
   }
 
-  findAll() {
-    return `This action returns all match`;
+  async findAll(): Promise<Match[]> {
+    return await this.matchRepository.findAll();
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} match`;
+  async findOne(id: string): Promise<Match> {
+    try {
+      const match = await this.matchRepository.findOneId(id);
+
+      if (!match) throw new NotFoundException();
+
+      return match;
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.path === '_id')
+        throw new BadRequestException('Type of id invalid');
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.status === 404)
+        throw new NotFoundException('Challenge not found');
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      throw new BadRequestException(error.message);
+    }
   }
 
   update(id: string, updateMatchDto: UpdateMatchDto) {
